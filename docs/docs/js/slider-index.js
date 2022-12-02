33492a4d5994dbb2217257b259b2a81e18038367
ghanmx <israelreyes16.ir@gmail.com>
2022-12-01 22:17:22 -0600

    call

---
+++ docs/docs/js/slider-index.js
@@ -0,0 +1,41 @@
+<<<<<<< HEAD
+$(document).ready(function() {
+  $("#js-main-slider")
+    .pogoSlider({
+      autoplay: true,
+      autoplayTimeout: 10000,
+      displayProgess: true,
+      preserveTargetSize: true,
+      targetWidth: 1000,
+      targetHeight: 300,
+      responsive: true
+    })
+    .data("plugin_pogoSlider");
+
+  var transitionDemoOpts = {
+    displayProgess: false,
+    generateNav: false,
+    generateButtons: false
+  };
+});
+=======
+$(document).ready(function() {
+  $("#js-main-slider")
+    .pogoSlider({
+      autoplay: true,
+      autoplayTimeout: 10000,
+      displayProgess: true,
+      preserveTargetSize: true,
+      targetWidth: 1000,
+      targetHeight: 300,
+      responsive: true
+    })
+    .data("plugin_pogoSlider");
+
+  var transitionDemoOpts = {
+    displayProgess: false,
+    generateNav: false,
+    generateButtons: false
+  };
+});
+>>>>>>> 45bbdbde7b586154b118f8ed47d3a04f10547f61

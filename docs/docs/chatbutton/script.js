801c437a777c9c756ab7cc4cdf0e912f5a08956d
ghanmx <91572299+ghanmx@users.noreply.github.com>
2022-12-16 08:16:52 -0600

    Delete docs directory

--- docs/docs/chatbutton/script.js
+++
@@ -1,9 +0,0 @@
-<<<<<<< HEAD
-
-$(document).ready(function(){var a=$("#chatbutton-wa");a.floatingWhatsApp({phone:a.attr("data-phone"),popupMessage:a.attr("data-popupmessage"),headerName:a.attr("data-headername"),headerTitle:a.attr("data-headertitle"),message:a.attr("data-message"),placeholder:a.attr("data-placeholder"),showPopup:a.attr("data-showpopup"),position:a.attr("data-position"),headerColor:a.attr("data-headerColor"),autoOpenTimeout:a.attr("data-autoOpenTimeout"),size:a.attr("data-size"),buttonImage:a.attr("data-image"),
-backgroundColor:a.attr("data-backgroundColor")})});
-=======
-
-$(document).ready(function(){var a=$("#chatbutton-wa");a.floatingWhatsApp({phone:a.attr("data-phone"),popupMessage:a.attr("data-popupmessage"),headerName:a.attr("data-headername"),headerTitle:a.attr("data-headertitle"),message:a.attr("data-message"),placeholder:a.attr("data-placeholder"),showPopup:a.attr("data-showpopup"),position:a.attr("data-position"),headerColor:a.attr("data-headerColor"),autoOpenTimeout:a.attr("data-autoOpenTimeout"),size:a.attr("data-size"),buttonImage:a.attr("data-image"),
-backgroundColor:a.attr("data-backgroundColor")})});
->>>>>>> 45bbdbde7b586154b118f8ed47d3a04f10547f61
